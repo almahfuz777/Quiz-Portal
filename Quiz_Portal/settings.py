546f238b1a739d21f3a0c54e40ee0776383fb59a
Arif Mainuddin <arifmainuddin18@gmail.com>
2024-12-30 20:16:17 +0600

    Update settings.py

--- Quiz_Portal/settings.py
+++ Quiz_Portal/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mohammed.mainuddin@northsouth.edu'
-EMAIL_HOST_PASSWORD = 'wmqo dbtr arsy qzpq'
+EMAIL_HOST_USER = ' ' #update it with yours
+EMAIL_HOST_PASSWORD = ' ' #update it with yours
 
 AUTH_USER_MODEL = 'core.User'  
 
